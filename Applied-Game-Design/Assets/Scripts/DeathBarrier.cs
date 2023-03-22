11d27d7a7094fccf9e333f23e81381c28690eaad
Emily van der Schaaf <emilyvdschaaf@gmail.com>
2023-03-22 19:24:36 +0100

    updated scene

--- Applied-Game-Design/Assets/Scripts/DeathBarrier.cs
+++ Applied-Game-Design/Assets/Scripts/DeathBarrier.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class DeathBarrier : MonoBehaviour
 {
+    [SerializeField] private List<GameObject> objectsToTurnOn = new List<GameObject>();
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.CompareTag("Player"))
         {
+            for (int i = 0; i < objectsToTurnOn.Count; i++)
+            {
+                objectsToTurnOn[i].SetActive(true);
+            }
             collision.GetComponent<CharacterController2D>().OnDeath();
         }
     }
