7f6955fe10bf29663fd662a8b6c05f1001a1d8f4
Emily van der Schaaf <emilyvdschaaf@gmail.com>
2023-03-31 12:48:08 +0200

    second platformer challenge spawns in sections and not all at once

--- Applied-Game-Design/Assets/Scripts/DeathBarrier.cs
+++ Applied-Game-Design/Assets/Scripts/DeathBarrier.cs
@@ -6,14 +6,24 @@ public class DeathBarrier : MonoBehaviour
 {
     [SerializeField] private List<GameObject> objectsToTurnOn = new List<GameObject>();
     public List<string> deathText = new List<string>();
+    public bool activateInSequence = false;
+    private int activationIndex;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.CompareTag("Player"))
+        if (collision.CompareTag("Player"))
         {
-            for (int i = 0; i < objectsToTurnOn.Count; i++)
+            if (activateInSequence && activationIndex < objectsToTurnOn.Count)
             {
-                objectsToTurnOn[i].SetActive(true);
+                objectsToTurnOn[activationIndex].SetActive(true);
+                activationIndex++;
+            }
+            else
+            {
+                for (int i = 0; i < objectsToTurnOn.Count; i++)
+                {
+                    objectsToTurnOn[i].SetActive(true);
+                }
             }
             collision.GetComponent<CharacterController2D>().OnDeath();
         }
